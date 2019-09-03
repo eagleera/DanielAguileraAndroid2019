f569676417ecabdf1584d967da7dd237e3001482
Eaglewas <daguilera3220@gmail.com>
2019-09-02 19:37:07 -0600

    se agrega splash screen

---
+++ app/src/main/java/com/example/danielaguileraandroid2019/SplashScreen.kt
@@ -0,0 +1,37 @@
+package com.example.danielaguileraandroid2019
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+
+class SplashScreen : Activity() {
+    private val SPLASH_TIME_OUT = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(
+            Runnable /*
+         * Showing splash screen with a timer. This will be useful when you
+         * want to show case your app logo / company
+         */
+
+            {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                val i = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(i)
+
+                // close this activity
+                finish()
+            }, SPLASH_TIME_OUT.toLong()
+        )
+    }
+}
