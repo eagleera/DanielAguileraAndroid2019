7cd8801485c96abb957d078eb59d3ce9756e6f4a
Eaglewas <daguilera3220@gmail.com>
2019-11-21 22:47:44 -0700

    se agrega firebase ejemplo

--- app/src/main/java/com/example/danielaguileraandroid2019/MainActivity.kt
+++ app/src/main/java/com/example/danielaguileraandroid2019/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.danielaguileraandroid2019
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +14,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("Hola", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = "el token es $token"
+                Log.w("Hola", "token:  $token")
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            })
 
         btnHelloWorld.setOnClickListener {
             val intent = Intent(this, HelloWorld::class.java)
---
+++ app/src/main/java/com/example/danielaguileraandroid2019/MyFirebaseMessagingService.kt
@@ -0,0 +1,6 @@
+package com.example.danielaguileraandroid2019
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+}
